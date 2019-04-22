4abfb653290afe27b38f05b5366a4c0511f2868e
Thiago Gabriel Irace Dias Rodrigues <181710103@senacinformatica.br>
2019-04-22 09:59:19 -0300

    ajuste menu

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
     <link rel="stylesheet" type="text/css" href="estilo.css">
   </head>
 
-  <body id="body_index">
-      <h1> Clinica Angels - Oftamologia </h1>
+  <body>
+
+      <?php
+      require_once 'menu.php';
+       ?>
 
   </body>
 </html>
--- menu.php
+++ menu.php
@@ -2,9 +2,9 @@
 
 <div id="menu_navegacao">
     <ul>
-        <li><a class="btn_3d" href="#">Início</a></li>
-        <li><a class="btn_3d" href="#">Planos</a></li>
-        <li><a class="btn_3d" href="#">App</a></li>
+        <li><a class="btn_3d" href="index.php">Início</a></li>
+        <li><a class="btn_3d" href="index.php">Planos</a></li>
+        <li><a class="btn_3d" href="index.php">App</a></li>
         <li><a class="btn_3d" href="cadastrar_consultas.php">Marcação</a></li>
     </ul>
 </div>
