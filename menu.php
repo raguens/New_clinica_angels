a2380d3b0d3b0e060e5c166306fc1965f306728f
Thiago Gabriel Irace Dias Rodrigues <181710103@senacinformatica.br>
2019-04-24 11:59:08 -0300

    Ajustes botões menu e estilizações

--- menu.php
+++ menu.php
@@ -1,8 +1,7 @@
 <div id="menu_navegacao">
     <ul>
-        <li><a class="btn_3d" href="#">Início</a></li>
-        <li><a class="btn_3d" href="index.php">Planos</a></li>
-        <li><a class="btn_3d" href="index.php">App</a></li>
+        <li><a class="btn_3d" href="index.php">Início</a></li>
+        <li><a class="btn_3d" href="#">App</a></li>
         <li><a class="btn_3d" href="cadastrar_consultas.php">Marcação</a></li>
         <li><a class="btn_3d" href="cadastrar_consultas.php">Consultas Marcadas</a></li>
     </ul>
